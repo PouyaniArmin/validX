3624793fe1c7eaa4da9ebf7619f729a557d8f552
PouyaniArmin <pouyaniarmin@gmail.com>
2025-08-02 17:04:37 +0330

    Implement core validation logic in Validation class

---
+++ src/Validation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Validx;
+
+use ReflectionClass;
+use Validx\Rules\RuleProcessor;
+
+use function PHPSTORM_META\map;
+
+use const Dom\VALIDATION_ERR;
+
+class Validation
+{
+    public function validate(array $data, array $rules)
+    {
+        $errors = [];
+        $split = fn($str, $separator) => array_map('trim', explode($separator, $str));
+        foreach ($rules as $field => $option) {
+            $fieldRules = $split($option, '|');
+            foreach ($fieldRules as $rule) {
+                $params = [];
+                if (strpos($rule, ":")) {
+                    [$rule_name, $param_str] = $split($rule, ":");
+                    $params = $split($param_str, ',');
+                } else {
+                    $rule_name = trim($rule);
+                }
+                $ruleReflection=new RuleProcessor;
+                $error=$ruleReflection->ruleName($rule_name)->applyRule($data,$field,$params);   
+                if (!empty($error)) {
+                    $errors[$field][]=$error;
+                }
+            }
+        }
+        return $errors;
+    }
+}

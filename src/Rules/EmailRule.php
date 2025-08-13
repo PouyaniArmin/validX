069d9f12aade4974d3b89ffd8b055989f2a36043
PouyaniArmin <pouyaniarmin@gmail.com>
2025-08-13 10:23:58 +0330

    remove comment

--- src/Rules/EmailRule.php
+++ src/Rules/EmailRule.php
@@ -8,9 +8,7 @@ class EmailRule implements RuleInterface{
 
     public function validate(array $data, string $field, ...$params): bool
     {
-        // fix
-        return filter_var($data[$field],FILTER_VALIDATE_EMAIL);
-        
+        return filter_var($data[$field],FILTER_VALIDATE_EMAIL);    
     }
     public function message(string $field, ...$params): string
     {

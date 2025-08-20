0b8a54889985c2a90cdc361a26c58dbb0343a736
PouyaniArmin <pouyaniarmin@gmail.com>
2025-08-20 08:48:59 +0330

    fix:namespace

--- src/Rules/EmailRule.php
+++ src/Rules/EmailRule.php
@@ -1,39 +1,39 @@
-    <?php
+<?php
 
-    namespace Validx\Rules;
+namespace Validx\Rules;
 
-    use Validx\Message\ErrorMessage;
+use Validx\Message\ErrorMessage;
 
+/**
+ * EmailRule
+ * 
+ * Validates whether a given field contains a valid email address.
+ */
+class EmailRule implements RuleInterface
+{
     /**
-     * EmailRule
+     * Validate if the field value is a valid email address.
+     *
+     * @param array $data   Input data array
+     * @param string $field Field name to validate
+     * @param mixed ...$params Additional parameters (not used here)
      * 
-     * Validates whether a given field contains a valid email address.
+     * @return bool True if valid email, false otherwise
      */
-    class EmailRule implements RuleInterface
+    public function validate(array $data, string $field, ...$params): bool
     {
-        /**
-         * Validate if the field value is a valid email address.
-         *
-         * @param array $data   Input data array
-         * @param string $field Field name to validate
-         * @param mixed ...$params Additional parameters (not used here)
-         * 
-         * @return bool True if valid email, false otherwise
-         */
-        public function validate(array $data, string $field, ...$params): bool
-        {
-            return filter_var($data[$field], FILTER_VALIDATE_EMAIL);
-        }
-        /**
-         * Get the error message for invalid email values.
-         *
-         * @param string $field Field name
-         * @param mixed ...$params Additional parameters (not used here)
-         * 
-         * @return string Error message
-         */
-        public function message(string $field, ...$params): string
-        {
-            return sprintf(ErrorMessage::EMAIL->getMessage(), ucfirst($field));
-        }
+        return filter_var($data[$field], FILTER_VALIDATE_EMAIL);
     }
+    /**
+     * Get the error message for invalid email values.
+     *
+     * @param string $field Field name
+     * @param mixed ...$params Additional parameters (not used here)
+     * 
+     * @return string Error message
+     */
+    public function message(string $field, ...$params): string
+    {
+        return sprintf(ErrorMessage::EMAIL->getMessage(), ucfirst($field));
+    }
+}
